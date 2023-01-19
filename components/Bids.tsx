c3ba100f5b788132eeea1cf49e7801596135a156
Rob <robertpollockbfs@gmail.com>
2023-01-19 18:17:08 +0000

    remove divs

--- components/Bids.tsx
+++ components/Bids.tsx
@@ -2,34 +2,26 @@ import { useFirestore, useFirestoreCollectionData } from "reactfire"
 import { collection, query } from "firebase/firestore"
 import { Bid as BidType } from "types/quest"
 import { UserCardBid } from "../components/UserCardBid"
-import { Grid, Box } from "@mui/material"
+import { Grid, CircularProgress, Typography } from "@mui/material"
 
 export function Bids({ path }): JSX.Element {
   const firestore = useFirestore()
   const questsQuery = query(collection(firestore, path))
-  const { status, data: bids } = useFirestoreCollectionData(questsQuery)
+  const { data: bids } = useFirestoreCollectionData(questsQuery)
+
+  if (!bids) {
+    return <CircularProgress />
+  }
 
   return (
-    <>
-      {status && (
-        <>
-          {status === "loading" ? (
-            <div>loading</div>
-          ) : (
-            <>
-              <Grid container columnSpacing={1} rowSpacing={4}>
-                {bids?.length ? (
-                  bids.map((bid: BidType, idx) => (
-                    <UserCardBid key={idx} value={bid}></UserCardBid>
-                  ))
-                ) : (
-                  <div>no bids</div>
-                )}
-              </Grid>
-            </>
-          )}
-        </>
+    <Grid container columnSpacing={1} rowSpacing={4}>
+      {bids?.length ? (
+        bids.map((bid: BidType, idx) => (
+          <UserCardBid key={idx} value={bid}></UserCardBid>
+        ))
+      ) : (
+        <Typography>no bids</Typography>
       )}
-    </>
+    </Grid>
   )
 }
